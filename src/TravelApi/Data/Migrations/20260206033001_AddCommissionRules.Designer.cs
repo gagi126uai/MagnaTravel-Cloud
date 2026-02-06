8bc2768b33dfccff39dd30be67ebdf9e978ef041
Gaston Albornoz <Gaston.AlbornozSalafia@alumnos.uai.edu.ar>
2026-02-06 00:30:19 -0300

    feat: Add migration for CommissionRules table

--- src/TravelApi/Data/Migrations/20260206033001_AddCommissionRules.Designer.cs
+++ src/TravelApi/Data/Migrations/20260206033001_AddCommissionRules.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TravelApi.Data;
@@ -11,9 +12,11 @@ using TravelApi.Data;
 namespace TravelApi.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260206033001_AddCommissionRules")]
+    partial class AddCommissionRules
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
